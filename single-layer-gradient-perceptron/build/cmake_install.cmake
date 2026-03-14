535e1b3259c1935cf1bc54a715da217ebe77db96
ironv <ironval09@gmail.com>
2026-03-14 14:01:08 +0100

    correction bug

--- single-layer-gradient-perceptron/build/cmake_install.cmake
+++ single-layer-gradient-perceptron/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/ironv/Documents/HEPL/MASI1/Q2/Réseaux de neurones articifiels/Projet/artificial-neural-network/single-layer-gradient-perceptron
+# Install script for directory: C:/Users/ironv/Documents/artificial-neural-network/single-layer-gradient-perceptron
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ironv/Documents/HEPL/MASI1/Q2/Réseaux de neurones articifiels/Projet/artificial-neural-network/single-layer-gradient-perceptron/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/ironv/Documents/artificial-neural-network/single-layer-gradient-perceptron/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
